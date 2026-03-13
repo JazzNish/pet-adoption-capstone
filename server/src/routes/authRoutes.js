3b8a951b666ba3d81b7a56e426b8d5689f365377
Jayson <jaysonguingab99@gmail.com>
2026-03-13 13:55:17 +0800

    Add secure admin login route

--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { sendOtpCode, googleAuth, verifyEmail, firebaseEmailLogin } from '../controllers/authController.js';
+import { sendOtpCode, googleAuth, verifyEmail, firebaseEmailLogin, adminLogin } from '../controllers/authController.js';
 
 const router = express.Router();
 
 router.post('/send-otp', sendOtpCode);
-router.post('/verify', verifyEmail)
+router.post('/verify', verifyEmail);
+router.post('/admin-login', adminLogin);
 router.post('/google', googleAuth);
 router.post('/firebase-login', firebaseEmailLogin);
 
